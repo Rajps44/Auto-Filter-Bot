eb70198a0e63698b21a7286accc141b147b49b03
Raj <singhayush88805@gmail.com>
2024-09-23 10:40:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -186,7 +186,7 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton("📍 ʜᴏᴡ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋ 📍", url=settings['tutorial'])
             ]]
-            await message.reply(f"[{get_size(files.file_size)}] {files.file_name}\n\nYour file is ready, Please get using this link. 👍", reply_markup=InlineKeyboardMarkup(btn), protect_content=settings['file_secure'])
+            await message.reply(f"[{get_size(files.file_size)}] {files.file_name}\n\nYour file is ready, Please get using this link. 👍", reply_markup=InlineKeyboardMarkup(btn), protect_content=False)
             return
     else:
         pass
